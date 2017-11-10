c90788579ced2809006ed2040dc541c41e75bf24
charles-d-burton <charles.burton@mindflash.com>
2017-11-10 14:45:42 -0700

    removed unused variables

--- config.tf
+++ config.tf
@@ -3,9 +3,6 @@ data "template_file" "vpn_config" {
   template = "${file("${path.module}/userdata.sh")}"
 
   vars {
-    #psk      = "${var.psk}"
     network_block = "10.0.0.0/8"
-    username      = "${var.username}"
-    password      = "${var.password}"
   }
 }
