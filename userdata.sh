a94163efe70753e4605602bc47ba5b50c23eb19d
charles-d-burton <charles.burton@mindflash.com>
2017-11-10 15:15:48 -0700

    added network variable

--- userdata.sh
+++ userdata.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/charles-d-burton/openvpn-install/master/openvpn-install.sh -O openvpn-install.sh && bash openvpn-install.sh > /root/vpnsetup.log
+wget https://raw.githubusercontent.com/charles-d-burton/openvpn-install/master/openvpn-install.sh -O openvpn-install.sh \
+&& NETWORK_ROUTE="${network_block}" bash openvpn-install.sh > /root/vpnsetup.log
 
