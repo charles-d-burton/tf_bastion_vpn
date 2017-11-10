a7d134d5aa5b41a6e7e561c3921a04310e3afb81
charles-d-burton <charles.burton@mindflash.com>
2017-11-09 17:10:49 -0700

    testing new vpn config

--- config.tf
+++ config.tf
@@ -1,10 +1,11 @@
 #The userdata script to start the instance with
 data "template_file" "vpn_config" {
-  template = "${file("${path.module}/userdata.sh")}"
+  template = "${file("${path.module}/create_ike.sh")}"
 
   vars {
-    psk      = "${var.psk}"
-    username = "${var.username}"
-    password = "${var.password}"
+    #psk      = "${var.psk}"
+    network_block = "10.0.0.0/8"
+    username      = "${var.username}"
+    password      = "${var.password}"
   }
 }
