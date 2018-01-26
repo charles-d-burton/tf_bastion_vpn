a9cb91130b3f8b2028a1424be181a115f0f0ed02
charles-d-burton <charles.burton@mindflash.com>
2018-01-26 16:49:47 -0700

    fix: `updated script`

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/charles-d-burton/openvpn-install/master/openvpn-install.sh -O openvpn-install.sh \
-&& NETWORK_ROUTE="${network_block}" GATEWAY=${gateway} CIDR=${cidr} NETMASK=${netmask} bash openvpn-install.sh > /root/vpnsetup.log
+export NETWORK_ROUTE=${network_block}
+export GATEWAY=${gateway}
+export CIDR=${cidr}
+export NETMASK=${netmask}
+wget https://raw.githubusercontent.com/charles-d-burton/openvpn-install/master/openvpn-install.sh -O /root/openvpn-install.sh
+chmod a+x /root/openvpn-install.sh
+/root/openvpn-install.sh > /root/vpnsetup.log
 
